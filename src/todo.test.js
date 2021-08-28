f68abbb57014c3d6fca4597663f73d23cf50550a
fe-olaf <frontend.olaf@gmail.com>
2021-08-29 08:59:07 +0900

    투두 예제 추가

---
+++ src/todo.test.js
@@ -0,0 +1,18 @@
+/**
+ * TODO: id: 1,2,3,4 number => id 값으로 수정 삭제 해보기, 텍스트, 완료 여부
+ */
+
+describe('TODO 테스트', () => {
+  it('추가 - 하나의 TODO 를 추가하면 todos 배열의 length 는 1 이 된다.', () => {
+    const todos = []
+  })
+  it('삭제 - 하나의 TODO 를 삭제하면 todos 배열의 length 는 0 이 된다. (값이 1나 추가된 상태여야한다)', () => {
+    const todos = []
+  })
+  it('수정 - 완료 상태가 false 값을 업데이트하면 true 가 된다.', () => {
+    const todos = []
+  })
+  it('필터링 - 완료 상태인 todos 의 length 는 3개 일 것이다. (완료 3개 미완료 1개 의 값을 추가한상태)', () => {
+    const todos = []
+  })
+})
